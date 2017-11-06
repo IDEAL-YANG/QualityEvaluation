f432251cb0a2a8ba348d17e5eec5411ecbf45410
lxyang6688@163.com <yanglixiang@cydeng.com>
2017-11-06 10:29:53 +0800

    Initial Commit

---
+++ QualityEvaluation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QualityEvaluation
+//
+//  Created by IDEAL YANG on 2017/11/6.
+//  Copyright © 2017年 IDEAL YANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
